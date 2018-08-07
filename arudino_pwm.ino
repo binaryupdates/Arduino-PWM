fb0bad85b0a2d7e057c5f2877126f1e9b2d8d867
BINARYUPDATES.COM <binaryupdates@gmail.com>
2018-08-07 15:41:44 +0530

    Add files via upload

---
+++ arudino_pwm.ino
@@ -0,0 +1,15 @@
+void setup() {
+}
+
+void loop() {
+  analogWrite(9, 0);
+  delay(1000);
+  analogWrite(9, 64);
+  delay(1000);
+  analogWrite(9, 127);
+  delay(1000);
+  analogWrite(9, 191);
+  delay(1000);
+  analogWrite(9, 255);
+  delay(1000);
+}
